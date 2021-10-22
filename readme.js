d1f707a6b6a51388828c6ca283ff780103555430
jendevane <87095302+jendevane@users.noreply.github.com>
2021-10-22 18:41:13 -0500

    added files

--- readme.js
+++ readme.js
@@ -1,5 +1,44 @@
-var inquirer = require('inquirer')
-inquirer
-    .prompt([
+var inquirer =require ('inquirer')
+const fs = require('fs');
+
+const questions = [
+    {
+        type: "input",
+        name: "name",
+        message: "Please enter your name"
+        },
+    {
+
+        type: "input",
+        name: "github name",
+        message: "Please enter your github name"
+        },
+    {
+    type: "input",
+    name: "title",
+    message: "Please enter the project title"
+    },
+    {
+    type: "input",
+    name: "description",
+    message: "Please enter a description of your project"
+    },
+    {
+        type: "input",
+        name: "instructions",
+        message: "Please provide instructions for your project"
+    },
+    {
+        type: "list",
+        name: "license",
+        message: "please choose a license",
+        choices: [
+            'MIT', 'Apache', 'Boost', 'BSD'
+        ], 
+        
+    },
+   
+
     
-])
+
+]
