9038fe9a9cd8cd879dfe36aa2a67df62f91c0515
jendevane <87095302+jendevane@users.noreply.github.com>
2021-10-24 20:35:02 -0500

    added code

--- readme.js
+++ readme.js
@@ -1,27 +1,29 @@
-var inquirer =require ('inquirer')
+var inquirer = require('inquirer');
 const fs = require('fs');
+const utils=require('utils')
+inquirer
+  .prompt([
 
-const questions = [
     {
         type: "input",
         name: "name",
         message: "Please enter your name"
-        },
+    },
     {
 
         type: "input",
         name: "github name",
         message: "Please enter your github name"
-        },
-    {
-    type: "input",
-    name: "title",
-    message: "Please enter the project title"
     },
     {
-    type: "input",
-    name: "description",
-    message: "Please enter a description of your project"
+        type: "input",
+        name: "title",
+        message: "Please enter the project title"
+    },
+    {
+        type: "input",
+        name: "description",
+        message: "Please enter a description of your project"
     },
     {
         type: "input",
@@ -32,13 +34,30 @@ const questions = [
         type: "list",
         name: "license",
         message: "please choose a license",
-        choices: [
-            'MIT', 'Apache', 'Boost', 'BSD'
-        ], 
+        choices: ['MIT','Mozilla','GNU'],
+      
+         
         
     },
    
+  ])
+  .then(function(data) {
+    // Bonus: Generate the name of your user file from their input
+    const filename =
+      data.name
+        .toLowerCase()
+        .split(' ')
+        .join('') + '.json';
+
+    fs.generateReadMe(filename, JSON.stringify(data, null, '\t'), function(err) {
+      if (err) {
+        return console.log(err);
+      }
+
+      console.log('Success!');
+    });
+  });
+
 
     
 
-]
